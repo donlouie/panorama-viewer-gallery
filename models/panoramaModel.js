1dc8b2312a7ac327a5ad54795aee32d0146abccc
Louie <dOnl0ui3.gall3g0s@gmail.com>
2021-04-29 18:53:43 +0800

    refactor index page

--- models/panoramaModel.js
+++ models/panoramaModel.js
@@ -8,13 +8,18 @@ const imageSchema = new mongoose.Schema({
 const panoramaSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: [true, 'Panorama must have a Title'],
   },
   location: {
     type: String,
-    required: true,
+    required: [true, 'Panorama must have a Location'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Panorama must have a Description'],
   },
   images: [imageSchema],
+  date: { type: Date, default: Date.now },
 });
 
 const Panorama = mongoose.model('Panorama', panoramaSchema);
